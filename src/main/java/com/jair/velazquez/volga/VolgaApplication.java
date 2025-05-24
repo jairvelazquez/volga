b29c01322bbbf701b25584198476fa40ee4df7a5
Itequiya <sadai.bp98@gmail.com>
2025-05-24 09:24:56 -0600

    Initial commit - Volga management webflux R2DBC project

---
+++ src/main/java/com/jair/velazquez/volga/VolgaApplication.java
@@ -0,0 +1,13 @@
+package com.jair.velazquez.volga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolgaApplication.class, args);
+	}
+
+}
